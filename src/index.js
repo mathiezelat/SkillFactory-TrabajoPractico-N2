12c9b05f6fa5029855aab46813af61e31bc1ebfe
Mathias Ezequiel Latronico <mathiezelat@gmail.com>
2022-07-23 18:40:38 -0300

    :sparkles: feat: add pug to project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
 
@@ -5,8 +6,17 @@ const app = express()
 
 app.set('port', 3000)
 
+app.engine('html', require('pug').renderFile)
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
 app.use(express.json())
 app.use(morgan('dev'))
+
+app.get('/', (req, res) => {
+	res.render('index')
+})
 
 app.listen(app.get('port'), () => {
 	console.log('🚀 Servidor escuchando en el puerto', app.get('port'))
